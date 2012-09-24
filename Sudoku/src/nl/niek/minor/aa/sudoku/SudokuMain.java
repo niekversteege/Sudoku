b916687fe118c39e7ad3d274bb864f168051abae
Niek <niekversteege@gmail.com>
2012-09-24 14:01:23 +0200

    init

---
+++ Sudoku/src/nl/niek/minor/aa/sudoku/SudokuMain.java
@@ -0,0 +1,15 @@
+package nl.niek.minor.aa.sudoku;
+
+public class SudokuMain
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
